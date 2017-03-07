f2649c329c36dbe438482dba495c9148402fedab
Jay Lowack <jl1421@att.com>
2017-03-07 08:22:26 -0600

    added required schema and functions

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,32 +13,53 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM matches;")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM players;")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT count(*) as num_players FROM players;")
+    row = c.fetchone()
+    return row[0]
+    conn.close()
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO players (name) VALUES (%s);", (name, ))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -47,6 +68,19 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    sql = """
+    SELECT p.player_id, p.name, w.win_tally, m.tot_num_matches
+    FROM players p
+    LEFT OUTER JOIN winners_tally w ON p.player_id=w.player_id
+    LEFT OUTER JOIN matches_tally m ON p.player_id=m.player_id;
+    """
+    c.execute(sql)
+    results = [(str(row[0]), row[1], int(row[2]), int(row[3]))
+               for row in c.fetchall()]
+    conn.close()
+    return results
 
 
 def reportMatch(winner, loser):
@@ -56,16 +90,22 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO matches (winner, loser) VALUES (%s, %s);",
+              [winner, loser])
+    conn.commit()
+    conn.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +113,23 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
+    conn = connect()
+    c = conn.cursor()
+    sql = """
+    SELECT p.player_id, p.name, w.win_tally
+    FROM players p
+    LEFT OUTER JOIN winners_tally w ON p.player_id=w.player_id
+    ORDER BY w.win_tally DESC;
+    """
+    results = []
+    c.execute(sql)
+    cnt = 1
+    for row in c.fetchall():
+        if cnt % 2 == 0:
+            results.append((player1_id, player1_name, row[0], row[1]))
+        else:
+            player1_id = row[0]
+            player1_name = row[1]
+        cnt += 1
+    conn.close()
+    return results
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,48 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+DROP VIEW IF EXISTS matches_tally;
+DROP VIEW IF EXISTS winners_tally;
+DROP VIEW IF EXISTS losers_tally;
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
 
+CREATE TABLE players (
+  player_id SERIAL primary key,
+  name text,
+);
 
+CREATE TABLE matches (
+  match_id SERIAL primary key,
+  winner integer,
+  loser integer
+);
+
+-- This view provides a list of each player with their total number of wins
+CREATE VIEW winners_tally
+  AS SELECT p.player_id,
+    SUM(CASE WHEN p.player_id = m.winner THEN 1 ELSE 0 END) AS win_tally
+    FROM players p
+    LEFT OUTER JOIN matches m ON p.player_id=m.winner
+    GROUP BY p.player_id;
+
+-- This view provides a list of each player with their total number of losses
+CREATE VIEW losers_tally
+  AS SELECT p.player_id,
+    SUM(CASE WHEN p.player_id = m.loser THEN 1 ELSE 0 END) AS loss_tally
+    FROM players p
+    LEFT OUTER JOIN matches m ON p.player_id=m.loser
+    GROUP BY p.player_id;
+
+-- This view provides a list of each player with their total number of matches
+-- It accomplishes this by summing their total wins and losses together
+CREATE VIEW matches_tally
+  AS SELECT player_id, SUM(match_tally) AS tot_num_matches
+  FROM (
+    SELECT player_id, win_tally AS match_tally
+    FROM winners_tally
+    UNION
+    SELECT player_id, loss_tally AS match_tally
+    FROM losers_tally
+  ) AS matches_tally
+  GROUP BY player_id;
